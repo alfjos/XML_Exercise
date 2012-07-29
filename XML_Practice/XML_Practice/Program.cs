12740588b1b131a1855c297846f02cbf7a0539d3
Alfredo José Ortiz Garcia <me@alfredortiz.com>
2012-07-29 05:32:13 -0600

    Created Project
    Created the project in Visual Studio 2010 as a Windows Form Application.
    Added the image XML_Practice_Sequence diagram.

---
+++ XML_Practice/XML_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace XML_Practice
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
